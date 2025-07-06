803e931cec07653509158d3f886166623b3f0c48
Nandika K <2nandika.k@gmail.com>
2025-07-06 19:48:06 -0400

    solved 206. reverse linkedlist

---
+++ ReverseLinkedList.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        if head is None or head.next is None:
+            return head
+        
+        node = ListNode(head.val)
+        curr = head.next
+        
+        while curr is not None:
+            node = ListNode(curr.val, node)
+            curr = curr.next
+            
+        return node
+        
