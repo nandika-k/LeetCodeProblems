bf748b501843e79ba2adfaad877ac2b691d47cc7
Nandika K <2nandika.k@gmail.com>
2025-07-06 20:38:42 -0400

    changed return procedure (similar runtime, better memory)

--- GroupAnagrams.py
+++ GroupAnagrams.py
@@ -14,3 +14,17 @@ class Solution:
             result.append(value)
         return result
 
+
+##second implementation, value list return is different
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+        
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word in groups:
+                groups[sorted_word].append(word)
+            else:
+                groups[sorted_word] = [word]
+
+        return [value for key,value in groups.items()]
