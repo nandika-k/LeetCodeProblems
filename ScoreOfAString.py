7926a5f8d33e8dbf8edd8044dad404565c4cc17a
Nandika K <2nandika.k@gmail.com>
2025-07-09 09:56:56 -0400

    better runtime

--- ScoreOfAString.py
+++ ScoreOfAString.py
@@ -6,3 +6,12 @@ class Solution:
             two = ord(s[i+1])
             score += abs(one - two)
         return score
+
+#still O(N) but better runtime
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        score = 0
+        lst = [ord(c) for c in s]
+        for i in range(len(lst) - 1):
+            score += abs(lst[i] - lst[i+1])
+        return score
