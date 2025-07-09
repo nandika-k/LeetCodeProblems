7a6f01784e0fa045474ebf0be9e29c9949d7ea4f
Nandika K <2nandika.k@gmail.com>
2025-07-09 09:59:00 -0400

    best combo of runtime and memory

--- ScoreOfAString.py
+++ ScoreOfAString.py
@@ -15,3 +15,11 @@ class Solution:
         for i in range(len(lst) - 1):
             score += abs(lst[i] - lst[i+1])
         return score
+    
+#finally something with better memory storage - modified version of original
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        score = 0
+        for i in range(len(s) - 1):
+            score += abs(ord(s[i]) - ord(s[i+1]))
+        return score
