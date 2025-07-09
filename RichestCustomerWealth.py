b45d4795109a3fda4fa494b2110d2b8831ba577a
Nandika K <2nandika.k@gmail.com>
2025-07-09 10:07:00 -0400

    Richest Customer Wealth - better memory than previous

--- RichestCustomerWealth.py
+++ RichestCustomerWealth.py
@@ -1,11 +1,21 @@
 #Beats 100% runtime, O(N) solution, 18.04MB memory
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        max = 0
+        maxWealth = 0
 
         for lst in accounts:
             wealth = sum(lst)
-            if wealth > max:
-                max = wealth
+            if wealth > maxWealth:
+                maxWealth = wealth
 
-        return max
+        return maxWealth
+    
+#slightly better memory-wise
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        maxWealth = 0
+
+        for lst in accounts:
+            maxWealth = max(sum(lst), maxWealth)
+
+        return maxWealth
