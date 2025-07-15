0739a8d61f639c9950dd27aa3f1bb5bb23ff2634
Nandika K <2nandika.k@gmail.com>
2025-07-15 10:51:34 -0400

    Problem 4. Median Of Two Sorted Arrays. Best runtime (beats 50ish%) but not great. Will revisit.

--- src/MedianOfTwoSortedArrays.py
+++ src/MedianOfTwoSortedArrays.py
@@ -10,41 +10,14 @@ use while loop to iterate through both lists until median is reached
 '''
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2) -> float:
-        medianPos = 1+(len(nums1) + len(nums2)) // 2
-        even = ((len(nums1) + len(nums2)) % 2 == 0)
-        count = 0
+        medianPos = (len(nums1) + len(nums2)) // 2
+        merged = nums1 + nums2
+        merged.sort()
 
-        lastElem = []
-
-        pos1 = 0
-        pos2 = 0
-
-        while pos1 < len(nums1) or pos2 < len(nums2):
-            smallerNum = -1
-            if pos1 >= len(nums1):
-                smallerNum = nums2[pos2]
-                pos2 += 1
-            elif pos2 >= len(nums2):
-                smallerNum = nums1[pos1]
-                pos1 += 1
-            else:
-                smallerNum = nums2[pos2]
-                if nums1[pos1] <= smallerNum:
-                    smallerNum = nums1[pos1]
-                    pos1 += 1
-                else:
-                    pos2 += 1
-
-            if len(lastElem) > 0 and not (even and len(lastElem) < 2):
-                lastElem.pop(0)
-            
-            lastElem.append(smallerNum)
-            count += 1
-
-            if count == medianPos and not even:
-                return lastElem[0]
-            elif count == medianPos:
-                return sum(lastElem) / 2
+        if len(merged) % 2 == 0:
+            return (merged[medianPos] + merged[medianPos-1])/2
+        else:
+            return merged[medianPos]
             
 sol = Solution()
 print(sol.findMedianSortedArrays([1,2,3,4,5,14], [6,7,8,9,10,11,12,13]))
