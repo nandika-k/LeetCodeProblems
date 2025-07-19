48fcd4a6ccb159a765a1abd21c72971601679c8c
Nandika K <2nandika.k@gmail.com>
2025-07-19 13:23:06 -0400

    242. Valid Anagram. More efficient version - O(26+N).

--- src/ValidAnagram.py
+++ src/ValidAnagram.py
@@ -1,3 +1,17 @@
+#More efficient version - O(26+N)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        alphabet = [0]*26
+
+        for a,b in zip(s,t):
+            alphabet[ord(a) - ord('a')] += 1
+            alphabet[ord(b) - ord('a')] -= 1
+
+        for freq in alphabet:
+            if freq != 0:
+                return False
+        return True
+
 #Readable version - O(26N)
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
